3e676a57c7fb813f9df6a688d0b048a1477ec93f
Sabir <90725119+MaskedOmega@users.noreply.github.com>
2025-05-25 06:49:56 +0100

    v2
    added additional features and better saving method and inclusive of multiple sprites animations types ie, idle walk etc.

--- SpriteSplitter.py
+++ SpriteSplitter.py
@@ -1,5 +1,7 @@
-from PIL import Image
+from PIL import Image 
 import numpy as np
+import os
+from pathlib import Path
 
 class SpriteSheet:
     def __init__(self):
@@ -18,7 +20,7 @@ class SpriteSheet:
         sprite = self.get_sprite(x, y, width, height)
         sprite.save(output_path)
 
-    def run(self,file_path,SpriteXY):
+    def run_single(self,file_path,SpriteXY):
         self.sprite_sheet = Image.open(file_path)
         self.SpriteXY = SpriteXY
         self.sprite_sheet_size = np.size(self.sprite_sheet)
@@ -35,8 +37,47 @@ class SpriteSheet:
                 count += 1
             pos +=1
 
+    def get_folders(self,look_path):
+        look_path = Path("The Male adventurer - Free")
+        folders = [folder.name for folder in look_path.iterdir() if folder.is_dir()]
+        return(folders)
 
+
+
+    def run_multi(self,SpriteXY,save_path,look_path):
+        folders = self.get_folders(look_path)
+
+
+        for f in range(len(folders)):
+            cobined_path = str(look_path) + "/" + str(folders[f]) +"/"+ str(folders[f]) + ".png"
+            
+            try:
+                self.sprite_sheet = Image.open(cobined_path)
+                self.SpriteXY = SpriteXY
+                self.sprite_sheet_size = np.size(self.sprite_sheet)
+                self.maxSizeX = self.sprite_sheet_size[0]
+                self.maxSizeY = self.sprite_sheet_size[1]
+                
+                pos = 0
+                count = 0
+
+                path = os.path.join(save_path, folders[f])
+                os.mkdir(path)
+                print("Directory '%s' created" % folders[f])
+
+                for i in range (0,self.maxSizeY,self.SpriteXY[1]):
+                    count = 0
+                    for j in range (0,self.maxSizeX,self.SpriteXY[0]):
+                        self.save_sprite(j,i,SpriteXY[0],SpriteXY[1], path + "/" + str(pos) + str(folders[f]) + str(count) + ".png")
+                        count += 1
+                    pos +=1
+            except:
+                print("skip")
 #testing:
 
-#a = SpriteSheet()
+a = SpriteSheet()
 #a.run("sprite/Idle/idle.png",[48,64])
+
+
+
+a.run_multi([48,64],"test","The Male adventurer - Free")
