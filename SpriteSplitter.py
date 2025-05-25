89d496b61a571bcd43b74f5ab69aacf5010a607b
Sabir <90725119+MaskedOmega@users.noreply.github.com>
2025-05-25 22:04:58 +0100

    Update SpriteSplitter.py

--- SpriteSplitter.py
+++ SpriteSplitter.py
@@ -38,46 +38,44 @@ class SpriteSheet:
             pos +=1
 
     def get_folders(self,look_path):
-        look_path = Path("The Male adventurer - Free")
-        folders = [folder.name for folder in look_path.iterdir() if folder.is_dir()]
+        look_path = Path(look_path)
+        folders = [folder.name.lower() for folder in look_path.iterdir() if folder.is_dir()]
         return(folders)
-
-
 
     def run_multi(self,SpriteXY,save_path,look_path):
         folders = self.get_folders(look_path)
 
-
         for f in range(len(folders)):
+
+            
             cobined_path = str(look_path) + "/" + str(folders[f]) +"/"+ str(folders[f]) + ".png"
             
-            try:
-                self.sprite_sheet = Image.open(cobined_path)
-                self.SpriteXY = SpriteXY
-                self.sprite_sheet_size = np.size(self.sprite_sheet)
-                self.maxSizeX = self.sprite_sheet_size[0]
-                self.maxSizeY = self.sprite_sheet_size[1]
-                
-                pos = 0
+
+            self.sprite_sheet = Image.open(cobined_path)
+            self.SpriteXY = SpriteXY
+            self.sprite_sheet_size = np.size(self.sprite_sheet)
+            self.maxSizeX = self.sprite_sheet_size[0]
+            self.maxSizeY = self.sprite_sheet_size[1]
+            
+            pos = 0
+            count = 0
+
+            path = os.path.join(save_path, folders[f])
+            os.mkdir(path)
+            print("Directory '%s' created" % folders[f])
+
+            for i in range (0,self.maxSizeY,self.SpriteXY[1]):
                 count = 0
+                for j in range (0,self.maxSizeX,self.SpriteXY[0]):
+                    self.save_sprite(j,i,SpriteXY[0],SpriteXY[1], path + "/" + str(folders[f]) +"_"+ str(pos) +"_"+ str(count) + ".png")
+                    count += 1
+                pos +=1
 
-                path = os.path.join(save_path, folders[f])
-                os.mkdir(path)
-                print("Directory '%s' created" % folders[f])
-
-                for i in range (0,self.maxSizeY,self.SpriteXY[1]):
-                    count = 0
-                    for j in range (0,self.maxSizeX,self.SpriteXY[0]):
-                        self.save_sprite(j,i,SpriteXY[0],SpriteXY[1], path + "/" + str(pos) + str(folders[f]) + str(count) + ".png")
-                        count += 1
-                    pos +=1
-            except:
-                print("skip")
 #testing:
 
-a = SpriteSheet()
+#a = SpriteSheet()
 #a.run("sprite/Idle/idle.png",[48,64])
 
 
 
-a.run_multi([48,64],"test","The Male adventurer - Free")
+#a.run_multi([48,64],"test","Sprite")
